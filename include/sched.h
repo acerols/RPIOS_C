1b495d37f33f60dd4b7b1953096929c199fa4dd5
Yuto Kaihara <arizexy@gmail.com>
2020-04-09 02:05:36 +0900

    new file:   include/sched.h

---
+++ include/sched.h
@@ -0,0 +1,59 @@
+#ifndef _SCHED_H
+#define _SCHED_H
+
+#include "typedef.h"
+
+#define THREAD_CPU_CONTEXT          0
+#ifndef __ASEMBLER__
+
+#define THREAD_SIZE                 4096
+#define FIRST_TASK                  task[0]
+#define LAST_TASK                   task[NR_TASKS-1]
+
+#define TASK_RUNNING                0
+
+extern struct task_struct *current;
+extern struct task_struct *task[NR_TASKS]
+extern int32_t nr_tasks;
+
+struct cpu_context{
+    uint64_t x19;
+    uint64_t x20;
+    uint64_t x21;
+    uint64_t x22;
+    uint64_t x23;
+    uint64_t x24;
+    uint64_t x25;
+    uint64_t x26;
+    uint64_t x27;
+    uint64_t x28;
+    uint64_t fp;
+    uint64_t sp;
+    uint64_t pc;
+};
+
+struct task_struct{
+    struct cpu_context cpu_context;
+    int64_t stat;
+    int64_t counter;
+    int64_t priority;
+    int64_t preempt_count;
+};
+
+extern void sched_init(void);
+extern void schedule(void);
+extern void timer_tick(void);
+extern void preempt_disable(void);
+extern void preempt_enable(void);
+extern void switch_to(struct task_struct *next);
+extern void cpu_switch_to(stuct task_struct* prev, strcut task_struct* next);
+
+#define INIT_TASK \
+/*cpu_context*/	{ {0,0,0,0,0,0,0,0,0,0,0,0,0}, \
+/* state etc */	0,0,1, 0 \
+}
+
+#endif
+
+
+#endif
