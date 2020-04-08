793f38702755917ca41f93415a817d53230e9b16
Yuto Kaihara <arizexy@gmail.com>
2020-04-08 14:32:45 +0900

    new file:   include/printf.h 	modified:   kernel8.img 	modified:   main.c 	new file:   printf.c 	new file:   sprintf.c 	new file:   sprintf.h 	modified:   start.S

---
+++ include/printf.h
@@ -0,0 +1,6 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
+void printf(char *fmt, ...);
+
+#endif
--- sprintf.h
+++ sprintf.h
@@ -23,49 +23,5 @@
  *
  */
 
-#include "uart.h"
-#include "rand.h"
-#include "timer.h"
-#include "typedef.h"
-#include "printf.h"
-
-#define IRQ_BASIC   ((volatile uint32_t *)(0x3F00B200))
-#define IRQ_PEND2   ((volatile uint32_t *)(0x3F00B208))
-#define IRQ_ENABLE2 ((volatile uint32_t *)(0x3F00B214))
-#define GPU_INTERRUPTS_ROUTING ((volatile uint32_t *)(0x4000000C))
-#define CORE0_INTERRUPT_SOURCE ((volatile uint32_t *)(0x40000060))
-
-void main()
-{
-    // set up serial console and random number generator
-    uart_init();
-    rand_init();
-    
-    // enable UART RX interrupt.
-    
-    *UART0_IMSC = 1 << 4;
-
-    // UART interrupt routing.
-    *IRQ_ENABLE2 = 1 << 25;
-
-    // IRQ routeing to CORE0.
-    *GPU_INTERRUPTS_ROUTING = 0x00;
-
-    /*
-    uart_puts("Here goes a random number: ");
-    uart_hex(rand(0,4294967295));
-    uart_puts("\n");
-    */
-
-    printf("printf\n");
-
-    init_timer(1);
-
-    enable_irq();
-
-    // echo everything back
-    while(1) {
-        uart_send(uart_getc());
-    }
-}
-
+unsigned int sprintf(char *dst, char* fmt, ...);
+unsigned int vsprintf(char *dst,char* fmt, __builtin_va_list args);
