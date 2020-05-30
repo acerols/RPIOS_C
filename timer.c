1c2be9f4fed89e57f2faba3310d6e4f27cff11a7
Yuto Kaihara <arizexy@gmail.com>
2020-05-30 16:25:43 +0900

    deleted:    include/localtimer.h 	modified:   kernel8.img 	modified:   localtimer.c 	modified:   timer.c

--- timer.c
+++ timer.c
@@ -76,7 +76,7 @@ void init_timer(uint32_t usec)
     //timer = cntfrq / 1e6;
     //timer *= usec;
     //write_cntv_tval(timer);
-
+    cntfrq = cntfrq >> 2;
     write_cntv_tval(cntfrq);
 
     routing_core0cntv_to_core0irq();
@@ -89,23 +89,3 @@ void set_timer(void)
     write_cntv_tval(cntfrq);
 }
 
-void init_localtimer()
-{
-    QA7->TimerRouting.Routing = LOCALTIMER_TO_CORE0_IRQ;			// Route local timer IRQ to Core0
-
-	QA7->TimerControlStatus.ReloadValue = 0xFFFF00;						// Timer period set
-	QA7->TimerControlStatus.TimerEnable = 1;						// Timer enabled
-    QA7->TimerControlStatus.IntEnable = 1;							// Timer IRQ enabled
-
-	QA7->TimerClearReload.IntClear = 1;								// Clear interrupt
-	QA7->TimerClearReload.Reload = 1;								// Reload now
-
-	QA7->Core0TimerIntControl.nCNTPNSIRQ_IRQ = 1;					// We are in NS EL1 so enable IRQ to core0 that level
-	QA7->Core0TimerIntControl.nCNTPNSIRQ_FIQ = 0;					// Make sure FIQ is zero
-}
-
-void set_localtimer()
-{
-    local_timer_clr_reload_reg_t temp = { .IntClear = 1, .Reload = 1 };
-	QA7->TimerClearReload  = temp;
-}
